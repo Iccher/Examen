8d14b74748b23e5445c8b8fdafc154aa28004d6d
Iccher Enrique Espinoza Ramirez <iccherenrique@gmail.com>
2013-04-09 09:59:09 -0500

    Exmanen 2 agregado

---
+++ RESTFulDemo/RESTFulDemo/IStudents.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace RESTFulDemo
+{
+    #region IStudents Interface
+    [ServiceContract]
+    public interface IStudents
+    {
+        [OperationContract]
+        //[WebGet(ResponseFormat = WebMessageFormat.Json)]
+        [WebGet]
+        string ObtenerSaludo();
+
+
+
+        //POST operation
+        [OperationContract]
+        [WebInvoke(UriTemplate = "", Method = "POST")]
+        Student CreateStudent(Student createStudent);
+
+        //Get Operation
+        [OperationContract]
+        [WebGet(UriTemplate = "")]
+        List<Student> GetAllStudent();
+        [OperationContract]
+        [WebGet(UriTemplate = "{id}")]
+        Student GetAStudent(string id);
+
+        //PUT Operation
+        [OperationContract]
+        [WebInvoke(UriTemplate = "{id}", Method = "PUT")]
+        Student UpdateStudent(string id, Student updateStudent);
+
+        //DELETE Operation
+        [OperationContract]
+        [WebInvoke(UriTemplate = "{id}", Method = "DELETE")]
+        void DeleteStudent(string id);
+
+    }
+    #endregion
+
+    #region Student Entity
+    [DataContract]
+    public class Student
+    {
+        [DataMember]
+        public string birthDate;
+        [DataMember]
+        public string firtsName;
+        [DataMember]
+        public string idBooster;
+        [DataMember]
+        public string lastName;
+
+    }
+    #endregion
+
+}
---
+++ RESTFulDemo/RESTFulDemo/Students.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+using System.ServiceModel.Activation;
+
+namespace RESTFulDemo
+{
+    /// <summary>
+    /// Basically this code is developed for HTTP GET, PUT, POST & DELETE operation.
+    /// </summary>
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+    public class Students : IStudents
+    {
+        List<Student> objStudent = new List<Student>();
+        int StudentCount = 0;
+
+        public Student CreateStudent(Student createStudent)
+        {
+            createStudent.idBooster = (++StudentCount).ToString();
+            objStudent.Add(createStudent);
+            return createStudent;
+        }
+
+        public List<Student> GetAllStudent()
+        {
+            return objStudent.ToList();
+        }
+
+        public Student GetAStudent(string id)
+        {
+            return objStudent.FirstOrDefault(e => e.idBooster.Equals(id));
+        }
+
+        public Student UpdateStudent(string id, Student updateStudent)
+        {
+            Student p = objStudent.FirstOrDefault(e => e.idBooster.Equals(id));
+            p.birthDate = updateStudent.birthDate;
+            p.firtsName = updateStudent.firtsName;
+            p.lastName = updateStudent.lastName;
+            return p;
+        }
+
+        public void DeleteStudent(string id)
+        {
+            objStudent.RemoveAll(e => e.idBooster.Equals(id));
+        }
+
+
+        public string ObtenerSaludo()
+        {
+            var hora = DateTime.Now.Hour;
+            if (hora < 12)
+                return "Buenos Dias";
+            else if (hora < 19)
+                return "Buenas Tardes";
+            else
+                return "Buenas Noches";
+        }
+    }
+}
